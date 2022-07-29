e3847cd7d70702275445869219ce9bcdff110137
Emiel <emiel92@gmail.com>
2022-07-29 12:42:13 +0200

    added extra main

--- src/main/java/nl/miw/groningen/ch9/sevenWonders/emiel/App.java
+++ src/main/java/nl/miw/groningen/ch9/sevenWonders/emiel/App.java
@@ -82,6 +82,6 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
---
+++ src/main/java/nl/miw/groningen/ch9/sevenWonders/emiel/NewMain.java
@@ -0,0 +1,12 @@
+package nl.miw.groningen.ch9.sevenWonders.emiel;
+
+/**
+ * @author Emiel Bloem
+ * <p>
+ * Dit doet het programma
+ */
+public class NewMain {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
