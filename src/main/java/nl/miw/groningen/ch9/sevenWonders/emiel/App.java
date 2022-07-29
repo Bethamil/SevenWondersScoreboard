888df76ea0fad9135839a4febe12bddde3ac73b5
Emiel <emiel92@gmail.com>
2022-07-29 18:05:49 +0200

    added icon and title

--- src/main/java/nl/miw/groningen/ch9/sevenWonders/emiel/App.java
+++ src/main/java/nl/miw/groningen/ch9/sevenWonders/emiel/App.java
@@ -4,13 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import nl.miw.groningen.ch9.sevenWonders.emiel.controller.FinalScoreController;
 import nl.miw.groningen.ch9.sevenWonders.emiel.controller.StatsController;
 import nl.miw.groningen.ch9.sevenWonders.emiel.databases.mysql.DBAccess;
 import nl.miw.groningen.ch9.sevenWonders.emiel.databases.mysql.PlayersDAO;
 import nl.miw.groningen.ch9.sevenWonders.emiel.model.GameResults;
+
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * SevenWondersScoreBoard JavaFX App. This app can replace your scorecard from the boardgames Seven Wonders Duel.
@@ -25,6 +28,9 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("menu"), 500, 760);
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass()
+                .getResource("/images/sevenWondersIcon.png")).toExternalForm()));
+        stage.setTitle("Seven Wonders Duel Scoreboard");
         stage.setScene(scene);
         stage.show();
     }
